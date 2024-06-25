bfca1eb2eccc7e1599098225e3b77a9b9288d6f0
Sanskriti <sanskritityagi2003@gmail.com>
2024-06-25 15:02:36 +0530

    added image

--- src/components/AllProjects/Logos.jsx
+++ src/components/AllProjects/Logos.jsx
@@ -1,12 +1,134 @@
 import React from 'react'
 import Logo from '../../images/Logo.png'
 import './Logo.css'
+import Multicorp from '../../images/Multicorp.png'
+import Chail from '../../images/Chail.png'
+import Castorsuites from '../../images/Castorsuites.png'
+import Logo1 from '../../images/Logo1.png'
+import Logo2 from '../../images/Logo2.png'
+import Guesthouse  from '../../images/Guesthouse.png'
+import Advalyze from '../../images/Advalyze.png'
+import Fixebuy from '../../images/Fixebuy.png'
+import Altech from '../../images/Altech.png'
+import Aqualsheel from '../../images/Aqualsheel.png'
+import Hapydose from '../../images/Hapydose.png'
+import Akoyamart from  '../../images/Akoyamart.png'
+import DFAA from '../../images copy/DFAA.png'
+
 const Logos = () => {
   return (
-    <div className='introduction'>
-        <h1>LOGOS</h1>
-        <img src={Logo} style={{width:"400px"}}/>
-    </div>
+    <>
+      <div className='introduction'>
+        <div className='intro'>
+          <h1>LOGOS</h1>
+          <img src={Logo} style={{ width: "450px" }} />
+        </div>
+
+        <div className='introduce'>
+          <div>We've been engaged in a variety of 
+projects lately, designing Mobile Apps, 
+Websites, Dashboards & developing 
+digital experiences. All our recent work reflects our commitment to helping 
+clients achieve their goals.</div>
+
+
+<p>We've been engaged in a variety of 
+projects lately, designing Mobile Apps, 
+Websites, Dashboards & developing 
+digital experiences. All our recent work reflects our commitment to helping 
+clients achieve their goals.</p>
+
+        </div>
+
+      </div >
+      <div className='cutter'></div>
+ 
+                 
+      <div className='logodesign'>
+                <div className='log'>
+                    < img src={Multicorp} style={{ height: 'auto', width: '100%', objectFit: 'contain', borderRadius: "10%" }} />
+                </div>
+                <div className='log'> 
+                    < img src={Chail} style={{ height: 'auto', width: '100%', objectFit: 'contain', borderRadius: "10%" }} />
+                   
+                </div>
+                <div className='log'> 
+                    < img src={Castorsuites} style={{ height: 'auto', width: '100%', objectFit: 'contain', borderRadius: "10%" }} />
+                   
+                </div>
+
+
+            </div>
+
+
+            <div className='logodesigns'>
+                <div className='log'>
+                    < img src={Logo1} style={{ height: 'auto', width: '100%', objectFit: 'contain', borderRadius: "10%" }} />
+                </div>
+                <div className='log'> 
+                    < img src={Logo2} style={{ height: 'auto', width: '100%', objectFit: 'contain', borderRadius: "10%" }} />
+                   
+                </div>
+                <div className='log'> 
+                    < img src={Guesthouse} style={{ height: 'auto', width: '100%', objectFit: 'contain', borderRadius: "10%" }} />
+                   
+                </div>
+
+                
+            </div>
+
+
+            
+            <div className='logdesign'>
+                <div className='log'>
+                    < img src={Advalyze} style={{ height: 'auto', width: '100%', objectFit: 'contain', borderRadius: "10%" }} />
+                </div>
+                <div className='log'> 
+                    < img src={Fixebuy} style={{ height: 'auto', width: '100%', objectFit: 'contain', borderRadius: "10%" }} />
+                   
+                </div>
+                <div className='log'> 
+                    < img src={Altech} style={{ height: 'auto', width: '100%', objectFit: 'contain', borderRadius: "10%" }} />
+                   
+                </div>
+
+                
+            </div>
+
+
+            <div className='logdesigns'>
+                <div className='log'>
+                    < img src={Aqualsheel} style={{ height: 'auto', width: '100%', objectFit: 'contain', borderRadius: "10%" }} />
+                </div>
+                <div className='log'> 
+                    < img src={Hapydose} style={{ height: 'auto', width: '100%', objectFit: 'contain', borderRadius: "10%" }} />
+                   
+                </div>
+                <div className='log'> 
+                    < img src={Akoyamart} style={{ height: 'auto', width: '100%', objectFit: 'contain', borderRadius: "10%" }} />
+                   
+                </div>
+
+                
+            </div>
+
+
+            <div className='logdesigns'>
+                <div className='log'>
+                    < img src={DFAA} style={{ height: 'auto', width: '100%', objectFit: 'contain', borderRadius: "10%" }} />
+                </div>
+                <div className='log'> 
+                    < img src={Logo2} style={{ height: 'auto', width: '100%', objectFit: 'contain', borderRadius: "10%" }} />
+                   
+                </div>
+                <div className='log'> 
+                    < img src={Guesthouse} style={{ height: 'auto', width: '100%', objectFit: 'contain', borderRadius: "10%" }} />
+                   
+                </div>
+
+                
+            </div>
+    </>
   )
 }
 
